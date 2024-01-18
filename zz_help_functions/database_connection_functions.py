edd4d1097e7c85f7d2fa723ca1af49dac7544d02
Simone <simone.calio@virgilio.it>
2024-01-18 21:48:14 +0100

    initial functions

--- zz_help_functions/database_connection_functions.py
+++ zz_help_functions/database_connection_functions.py
@@ -1,16 +1,33 @@
-import pandas as pd
+#> define the correct folderpath
 
+def set_folerpath():
+    
+    import getpass
+    
+    username = getpass.getuser()
+    
+    if username == 'simon':
+        folderPath = 'C:/Users/simon/Desktop/github_projects/hands_on_statistics/'
+    else:
+        folderPath = '/Users/tugbasabanoglu/Desktop/hands_on_statistics/'
+    
+    return folderPath
+
+
+
+#> oecd connector function
 
 def get_from_oecd(sdmx_query):
+
+    """
+    function to extract data from the OECD API. Input --> indicator's code
+    """
+    
+    import pandas as pd
+    
     return pd.read_csv(
-        f"https://stats.oecd.org/SDMX-JSON/data/{sdmx_query}?contentType=csv"
-    )
+        f"https://stats.oecd.org/SDMX-JSON/data/{sdmx_query}?contentType=csv")
 
 
 
 
-# data = get_from_oecd('EAG_TRANS_DURUNEMP?COUNTRY=AUS+USA&AGE=T')
-
-# # test = 'https://stats.oecd.org/SDMX-JSON/data/OECD.EDU.IMEP/DSD_EAG_UOE_FIN@DF_UOE_FIN_NATURE_CUR_CAP?contentType=csv'
-
-# https://stats.oecd.org/SDMX-JSON/data/EAG_TRANS_DURUNEMP?COUNTRY=AUS+USA&AGE=T?contentType=csv
